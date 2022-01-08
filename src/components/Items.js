9f35f957a5bb7672e3189b12b0a135f0b49c4e52
Ian Boyle <irboyle21@gmail.com>
2022-01-08 13:28:45 -0500

    adds strikeThrough function to Items component

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Items(props) {
+  const [taskCompleted, setTaskCompleted] = useState(false);
+  function strikeThrough() {
+    if (taskCompleted === false) {
+      setTaskCompleted(true);
+    } else {
+      setTaskCompleted(false);
+    }
+  }
+  function unStrike() {
+    setTaskCompleted(false);
+  }
   return (
     <div>
-      <li>{props.text}</li>
+      <li onClick={strikeThrough} style={{ textDecoration: taskCompleted ? "line-through" : null }}>
+        {props.text}
+      </li>
     </div>
   );
 }
