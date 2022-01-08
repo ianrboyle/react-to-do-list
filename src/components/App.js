f7042eb960a15155d1cb1973f7d51e44bbcbce84
Ian Boyle <irboyle21@gmail.com>
2022-01-08 14:09:44 -0500

    changes where some of the functions live - primarily moves some functions from Form comp to App comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "./Heading";
 import Form from "./Form";
 import Items from "./Items";
 
 function App() {
+  const [items, setItems] = useState([]);
+  function addItem(inputText) {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+  }
+  function deleteItem(id) {
+    console.log("trying to dlete");
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div className="container">
       <Heading />
-      <Form />
+      <Form onAdd={addItem} />
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <Items key={index} id={index} text={todoItem} completed={deleteItem} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,41 +1,26 @@
 import React, { useState } from "react";
 import Items from "./Items";
 
-function Form() {
+function Form(props) {
   const [inputText, setInputText] = useState("");
-  const [items, setItems] = useState([]);
+
   function handleChange(event) {
     const newInput = event.target.value;
     setInputText(newInput);
   }
 
-  function addItem() {
-    setItems((prevItems) => {
-      return [...prevItems, inputText];
-    });
-  }
-  function deleteItem(id) {
-    console.log("trying to dlete");
-    setItems((prevItems) => {
-      return prevItems.filter((item, index) => {
-        return index !== id;
-      });
-    });
-  }
   return (
     <div>
       <div className="form">
         <input onChange={handleChange} value={inputText} type="text" />
-        <button onClick={addItem}>
+        <button
+          onClick={() => {
+            props.onAdd(inputText);
+            setInputText("");
+          }}
+        >
           <span>Add</span>
         </button>
-      </div>
-      <div>
-        <ul>
-          {items.map((todoItem, index) => (
-            <Items key={index} id={index} text={todoItem} completed={deleteItem} />
-          ))}
-        </ul>
       </div>
     </div>
   );
